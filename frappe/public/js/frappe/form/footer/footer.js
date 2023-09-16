f90566efdc0f0ccbbf656618d37ea2ef1b110860
Erpfit Com <erpfitcom@gmail.com>
2023-09-16 23:57:04 +0700

    feat: nested comment

--- frappe/public/js/frappe/form/footer/footer.js
+++ frappe/public/js/frappe/form/footer/footer.js
@@ -19,8 +19,9 @@ frappe.ui.form.Footer = class FormFooter {
 		});
 	}
 	make_comment_box() {
+		let comment_box = this.wrapper.find(".comment-box");
 		this.frm.comment_box = frappe.ui.form.make_control({
-			parent: this.wrapper.find(".comment-box"),
+			parent: comment_box,
 			render_input: true,
 			only_input: true,
 			enable_mentions: true,
@@ -31,10 +32,13 @@ frappe.ui.form.Footer = class FormFooter {
 			on_submit: (comment) => {
 				if (strip_html(comment).trim() != "" || comment.includes("img")) {
 					this.frm.comment_box.disable();
+					let reply_for = comment_box.find(".reply-for[data-comment]");
+					let reply_for_name = reply_for ? reply_for.attr("data-comment") : "";
+					let is_nested_comment = reply_for.length > 0 && reply_for_name;
 					frappe
 						.xcall("frappe.desk.form.utils.add_comment", {
-							reference_doctype: this.frm.doctype,
-							reference_name: this.frm.docname,
+							reference_doctype: is_nested_comment ? "Comment" : this.frm.doctype,
+							reference_name: is_nested_comment ? reply_for_name : this.frm.docname,
 							content: comment,
 							comment_email: frappe.session.user,
 							comment_by: frappe.session.user_fullname,
@@ -44,12 +48,36 @@ frappe.ui.form.Footer = class FormFooter {
 								this.frm.timeline.get_comment_timeline_item(comment);
 							this.frm.comment_box.set_value("");
 							frappe.utils.play_sound("click");
-							this.frm.timeline.add_timeline_item(comment_item);
+							if (is_nested_comment) {
+								$(`.timeline-item[data-name="${reply_for_name}"]`).after(
+									`<div class="timeline-item" data-doctype="Comment" data-name="${
+										comment_item.name
+									}">
+										<div class="timeline-badge" title="SmallMessage">
+											<svg class="icon  icon-md" style="">
+												<use class="" href="#icon-small-message"></use>
+											</svg>
+										</div>
+										<div class="timeline-content frappe-card" data-name="comment-${
+											comment_item.name
+										}" style="margin-left: 60px;">
+											<div class="timeline-message-box">
+												${comment_item.content.html()}
+											</div>
+										</div>
+									</div>`
+								);
+							} else {
+								this.frm.timeline.add_timeline_item(comment_item);
+							}
 							this.frm.sidebar.refresh_comments_count &&
 								this.frm.sidebar.refresh_comments_count();
 						})
 						.finally(() => {
 							this.frm.comment_box.enable();
+							if (is_nested_comment) {
+								reply_for.remove();
+							}
 						});
 				}
 			},
